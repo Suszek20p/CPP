c82f2c1018c44eeb6bc17f553f30d1e73f2b5f1b
Suszek20p <91263488+Suszek20p@users.noreply.github.com>
2022-04-26 14:04:00 +0200

    Update ip.cpp

--- IP/ip.cpp
+++ IP/ip.cpp
@@ -1,30 +1,44 @@
+// WCZYTAĆ ADRES IP I MASKĘ NP.
+//              172.168.10.10
+//              255.255.255.0
+// 1) WYŚWIETLIĆ IP I MASKĘ W FORMIE BINARNEJ
+// 2) WYŚWIETLIĆ KLASE IP I ILOŚĆ PODSIECI DLA MASKI KLASOWEJ
+// 3) WYŚWIETLIĆ ADRES SIECI HOSTÓW W PODSIECI
+// 4) SPRAWDZIĆ POPRAWNOŚĆ IP ORAZ MASKI
+
 #include <iostream>
 using namespace std;
 
-int Wczytywanie()
+struct IP
 {
     string ip;
     string maska;
+};
 
-    cout << "Podaj IP: " << endl;
-    cin >> ip;
-    cout << "Podaj Maske: " << endl;
-    cin >> maska;
-
-    cout << "Twoje IP: " <<  ip << endl;
-    cout << "Twoja Maska: " << maska << endl;
-
-    return 0;
-}
-
-int CzyLiczba()
+bool CzyLiczba(string ip)
 {
     
+    cout << "Podaj IP (np.: 255.255.255.255): " << endl;
+    cin >> ip;
+
+    for(int i; i > ip.length(); i++)
+    {
+        if(ip[i]<'0' || ip[i]>'9')
+        {
+            if(ip[i]!='.')
+            {
+                return "nie ma kropek";
+            }
+            return "Jest git";
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char const *argv[])
 {
-    Wczytywanie();
+    string ip;
+    CzyLiczba(ip);
 
     return 0;
 }
